fd2b4daee1287f6486af15e1a678bc06bfaebd2f
Stefano Gottardo <gottardo.stefano.83@gmail.com>
2018-10-28 11:41:02 +0100

    Project: Example: Added an example to manage events within a UserControl

--- Example/Controls/ExampleControl.xaml.cs
+++ Example/Controls/ExampleControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using WPF.MDI;
 
 namespace Example.Controls
 {
@@ -7,6 +8,9 @@ namespace Example.Controls
     /// </summary>
     public partial class ExampleControl : UserControl
     {
+        MdiChild MdiChild_Parent = null;
+        private int ActivatedCounter;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ExampleControl"/> class.
         /// </summary>
@@ -16,6 +20,27 @@ namespace Example.Controls
 
             Width = double.NaN;
             Height = double.NaN;
+        }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //This is a simple example to manage the events of the parent MdiChild directly within the UserControl
+
+            //Get the parent MdiChild
+            MdiChild_Parent = Helper.FindParent<MdiChild>(this);
+
+
+            //Set the event handlers
+            if (MdiChild_Parent != null)
+            {
+                MdiChild_Parent.Activated += MdiChild_Parent_Activated;
+            }
+        }
+
+        private void MdiChild_Parent_Activated(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ActivatedCounter += 1;
+            LB_ActivatedCounter.Content = "Activated event counter: " + ActivatedCounter + " times";
         }
     }
 }
